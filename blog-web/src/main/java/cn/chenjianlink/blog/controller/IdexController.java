ecd1067df26f571fcf0877b2d075d6ba784540b8
chenjian <chenjianlink1998@gmail.com>
2018-12-28 23:40:26 +0800

    对链接管理初步设置

--- blog-web/src/main/java/cn/chenjianlink/blog/controller/IdexController.java
+++ blog-web/src/main/java/cn/chenjianlink/blog/controller/IdexController.java
@@ -1,8 +1,13 @@
 package cn.chenjianlink.blog.controller;
 
+import cn.chenjianlink.blog.pojo.Link;
+import cn.chenjianlink.blog.service.LinkService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 
 /**
@@ -11,13 +16,18 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class IdexController {
 
+    @Resource
+    private LinkService linkService;
+
     //首页展示
     @RequestMapping("/index")
-    public ModelAndView index() {
+    public ModelAndView index() throws Exception {
         ModelAndView mav = new ModelAndView();
         mav.addObject("pageTitle", "局外人之个人空间");
         mav.addObject("mainPage", "foreground/blog/list.jsp");
         mav.setViewName("mainTemp");
+        List<Link> linkList = linkService.getLinkList();
+        mav.addObject("linkList", linkList);
         return mav;
     }
 
---
+++ blog-web/src/main/java/cn/chenjianlink/blog/controller/admin/LinkController.java
@@ -0,0 +1,29 @@
+package cn.chenjianlink.blog.controller.admin;
+
+import cn.chenjianlink.blog.common.utils.JsonUtils;
+import cn.chenjianlink.blog.pojo.Link;
+import cn.chenjianlink.blog.service.LinkService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * 友情链接管理Controller
+ */
+@Controller
+public class LinkController {
+
+    @Resource
+    private LinkService linkService;
+
+    @RequestMapping("/admin/link/list")
+    @ResponseBody
+    public String getLinkList() throws Exception {
+        List<Link> linkList = linkService.getLinkList();
+        Link link = linkList.get(1);
+        return JsonUtils.objectToJson(link);
+    }
+}
