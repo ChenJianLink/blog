e598f1c4ba147fccb38c8b67697c01ecd2c1e4c6
chenjian <chenjianlink1998@gmail.com>
2018-12-29 20:55:24 +0800

    所有博客页面添加友情链接

--- blog-web/src/main/java/cn/chenjianlink/blog/controller/BloggerController.java
+++ blog-web/src/main/java/cn/chenjianlink/blog/controller/BloggerController.java
@@ -1,8 +1,13 @@
 package cn.chenjianlink.blog.controller;
 
+import cn.chenjianlink.blog.pojo.Link;
+import cn.chenjianlink.blog.service.LinkService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * 查看博主信息Controller
@@ -10,15 +15,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BloggerController {
 
+    @Resource
+    private LinkService linkService;
+
     /**
      * "关于博主"页面展示
+     *
      * @param model
      * @return
      */
     @RequestMapping("/blogger/aboutMe")
-    public String aboutMe(Model model){
+    public String aboutMe(Model model) throws Exception {
         model.addAttribute("mainPage", "foreground/blogger/info.jsp");
         model.addAttribute("pageTitle", "关于博主-局外人之个人空间");
+        List<Link> linkList = linkService.getLinkList();
+        model.addAttribute("linkList", linkList);
         return "mainTemp";
     }
 
--- blog-web/src/main/java/cn/chenjianlink/blog/controller/MessageController.java
+++ blog-web/src/main/java/cn/chenjianlink/blog/controller/MessageController.java
@@ -1,19 +1,30 @@
 package cn.chenjianlink.blog.controller;
 
+import cn.chenjianlink.blog.pojo.Link;
+import cn.chenjianlink.blog.service.LinkService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * 留言板块Controller
  */
 @Controller
 public class MessageController {
+
+    @Resource
+    private LinkService linkService;
+
     //留言板展示
     @RequestMapping("/message/leavemessages")
-    public String messageboard(Model model){
+    public String messageboard(Model model) throws Exception{
         model.addAttribute("mainPage", "foreground/message/messagesboard.jsp");
         model.addAttribute("pageTitle", "游客留言-局外人之个人空间");
+        List<Link> linkList = linkService.getLinkList();
+        model.addAttribute("linkList", linkList);
         return "mainTemp";
     }
 }
