be1bd4411d2127021874e8dc3e92fb1c2fa11891
chenjian <chenjianlink1998@gmail.com>
2019-01-24 13:10:59 +0800

    实现在地址栏直接访问jsp页面的过滤器

---
+++ blog-web/src/main/java/cn/chenjianlink/blog/filter/JspFilter.java
@@ -0,0 +1,37 @@
+package cn.chenjianlink.blog.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Jsp页面直接访问过滤器
+ */
+public class JspFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        HttpSession session = httpServletRequest.getSession(true);
+        String url = httpServletRequest.getRequestURI();
+        session.setAttribute("L'Eteranger", "L'Eteranger");
+        System.out.println(url);
+        if (url != null && url.endsWith(".jsp")) {
+            httpServletResponse.sendRedirect("/index.html");
+        }
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
