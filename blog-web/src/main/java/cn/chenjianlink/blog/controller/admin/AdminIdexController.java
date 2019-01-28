23c5edeabbe0ee28d616bbb8c95c786dbe1c83ec
chenjian <chenjianlink1998@gmail.com>
2019-01-28 14:04:56 +0800

    完成写博客页面的博客类别选择的显示

--- blog-web/src/main/java/cn/chenjianlink/blog/controller/admin/AdminIdexController.java
+++ blog-web/src/main/java/cn/chenjianlink/blog/controller/admin/AdminIdexController.java
@@ -1,12 +1,15 @@
 package cn.chenjianlink.blog.controller.admin;
 
+import cn.chenjianlink.blog.pojo.BlogType;
 import cn.chenjianlink.blog.pojo.Blogger;
+import cn.chenjianlink.blog.service.BlogTypeService;
 import cn.chenjianlink.blog.service.BloggerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * 后台首面controller
@@ -19,6 +22,7 @@ public class AdminIdexController {
 
     @RequestMapping("/admin/blogger/admin-index")
     public String adminIndex(Model model) throws Exception {
+        //用户信息
         Blogger blogger = bloggerService.findBloggerAll();
         model.addAttribute("currentUser", blogger);
         return "admin/main";
--- blog-web/src/main/java/cn/chenjianlink/blog/controller/admin/ForwordController.java
+++ blog-web/src/main/java/cn/chenjianlink/blog/controller/admin/ForwordController.java
@@ -1,16 +1,32 @@
 package cn.chenjianlink.blog.controller.admin;
 
+import cn.chenjianlink.blog.pojo.BlogType;
+import cn.chenjianlink.blog.service.BlogTypeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * 后台jsp页面跳转显示Controller
  */
 @Controller
 public class ForwordController {
+
+    @Resource
+    private BlogTypeService blogTypeService;
+
     @RequestMapping("/admin/{page}")
-    public String forwordPage(@PathVariable String page) {
+    public String forwordPage(@PathVariable String page, Model model) throws Exception {
+        //判断是否为写博客的页面,是则向页面添加博客类别
+        if (page.equals("writeBlog")) {
+            //添加博客类别
+            List<BlogType> blogTypeCountList = blogTypeService.getBlogTypeCountList();
+            model.addAttribute("blogTypeCountList", blogTypeCountList);
+        }
         return "/admin/" + page;
     }
 }
