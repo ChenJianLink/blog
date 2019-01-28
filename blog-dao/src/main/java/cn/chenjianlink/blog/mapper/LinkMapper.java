ab9ba9dce8ffc8a7b67e5b8e5f9c8267c432e64d
chenjian <chenjianlink1998@gmail.com>
2019-01-28 10:25:11 +0800

    添加注释

--- blog-dao/src/main/java/cn/chenjianlink/blog/mapper/LinkMapper.java
+++ blog-dao/src/main/java/cn/chenjianlink/blog/mapper/LinkMapper.java
@@ -8,13 +8,18 @@ import java.util.List;
  * 友情链接mapper
  */
 public interface LinkMapper {
+    //查询所有链接
     List<Link> selectAll() throws Exception;
 
+    //插入新链接
     int insert(Link link) throws Exception;
 
+    //根据id查询链接
     Link selectByPrimaryKey(int id) throws Exception;
 
+    //更新链接
     int update(Link link) throws Exception;
 
+    //删除链接
     int delete(int[] ids) throws Exception;
 }
