a4814b509905e54d2e65584f50fc439d28001546
chenjian <chenjianlink1998@gmail.com>
2019-01-30 14:05:50 +0800

    日期格式化

---
+++ blog-web/src/main/webapp/static/js/common.js
@@ -0,0 +1,29 @@
+//日期格式化
+Date.prototype.format = function (format) {
+    var o = {
+        "M+": this.getMonth() + 1, //month
+        "d+": this.getDate(), //day
+        "h+": this.getHours(), //hour
+        "m+": this.getMinutes(), //minute
+        "s+": this.getSeconds(), //second
+        "q+": Math.floor((this.getMonth() + 3) / 3), //quarter
+        "S": this.getMilliseconds() //millisecond
+    };
+    if (/(y+)/.test(format)) {
+        format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    }
+    for (var k in o) {
+        if (new RegExp("(" + k + ")").test(format)) {
+            format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
+        }
+    }
+    return format;
+};
+
+var BLOG = {
+    // 格式化时间
+    formatDateTime: function (val, row) {
+        var now = new Date(val);
+        return now.format("yyyy-MM-dd hh:mm:ss");
+    }
+}
