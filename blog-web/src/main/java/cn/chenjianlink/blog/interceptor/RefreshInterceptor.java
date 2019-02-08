5a316100293b57c8620ac84531c14c9a255d2b65
chenjian <chenjianlink1998@gmail.com>
2019-02-08 11:56:49 +0800

    去除多余的代码

--- blog-web/src/main/java/cn/chenjianlink/blog/interceptor/RefreshInterceptor.java
+++ blog-web/src/main/java/cn/chenjianlink/blog/interceptor/RefreshInterceptor.java
@@ -30,7 +30,6 @@ public class RefreshInterceptor implements HandlerInterceptor {
         if (ip == null || ip.length() == 0 || " unknown ".equalsIgnoreCase(ip)) {
             ip = request.getRemoteAddr();
         }
-        System.out.println("ip为" + ip);
         //开始进行判断
         HttpSession session = request.getSession();
         Ip ipo = (Ip) session.getAttribute(ip);
@@ -41,12 +40,10 @@ public class RefreshInterceptor implements HandlerInterceptor {
             ipoo.setTime(System.currentTimeMillis());
             ipoo.setRecount(1);
             session.setAttribute(ip, ipoo);
-            System.out.println("通过！");
             return true;
         } else {
             //如果ip不为空则禁止访问(恶意刷新后把ip存到session中)
             if (ipo.getIp() != null) {
-                System.out.println("session消失前永远被拦截了");
                 request.getRequestDispatcher("/jsp/foreground/error/errorforbid.jsp").forward(request, response);
                 return false;
             } else {
@@ -57,7 +54,6 @@ public class RefreshInterceptor implements HandlerInterceptor {
                     Ip ipoo = new Ip();
                     ipo.setIp(ip);
                     session.setAttribute(ip, ipoo);
-                    System.out.println("刷新过快");
                     request.getRequestDispatcher("/jsp/foreground/error/errorforbid.jsp").forward(request, response);
                 } else {
                     //当前时间减去session中的时间大于两秒的
@@ -67,25 +63,22 @@ public class RefreshInterceptor implements HandlerInterceptor {
                         ipoo.setTime(System.currentTimeMillis());
                         ipoo.setRecount(1);
                         session.setAttribute(ip, ipoo);
-                        System.out.println("大于两秒");
                         return true;
                     } else {
                         //小于两秒的
                         if (ipo.getRecount() > 12) {
                             //小于2秒，并且2秒间隔之内请求了12次
-                            System.out.println("进行拦截！");
                             //将ip放到session中用来禁止该ip访问
                             ipo.setIp(ip);
                             //将ip存到数据库里用来后台查看，如发现多次攻击将永远禁止访问
                             //获取ip所在区域
                             Ip jdip = new Ip();
                             AddressUtils addutil = new AddressUtils();
-                            System.out.println(addutil.getAddresses("ip=" + ip, "utf-8"));
                             //获取地址
-                            String loacl = addutil.getAddresses("ip=" + ip, "utf-8");
+                            String local = addutil.getAddresses("ip=" + ip, "utf-8");
                             //写入ip类
                             jdip.setCreatTime(new Date());
-                            jdip.setLocation(loacl);
+                            jdip.setLocation(local);
                             jdip.setIp(ip);
                             //userService.insertIp(jdip);
                             request.getRequestDispatcher("/jsp/foreground/error/errorforbid.jsp").forward(request, response);
@@ -94,7 +87,6 @@ public class RefreshInterceptor implements HandlerInterceptor {
                             //小于2秒，但请求数小于12次，给对象添加
                             ipo.setTime(System.currentTimeMillis());
                             ipo.setRecount(ipo.getRecount() + 1);
-                            System.out.println("访问次数" + ipo.getRecount());
                             request.getSession().setAttribute(ip, ipo);
                         }
                     }
