4d4f19ab24f4f2ccc7eb20660234fd971606aa4b
chenjian <chenjianlink1998@gmail.com>
2019-02-09 12:41:07 +0800

    去除不必要的导入

--- blog-web/src/main/java/cn/chenjianlink/blog/controller/BlogController.java
+++ blog-web/src/main/java/cn/chenjianlink/blog/controller/BlogController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.List;
 
 /**
  * 日志相关展示Controller
