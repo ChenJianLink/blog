3519898dc240506a09475f8cfde69ba26afa32f2
chenjian <chenjianlink1998@gmail.com>
2019-02-11 14:17:08 +0800

    根据留言页面创建留言对象

---
+++ blog-pojo/src/main/java/cn/chenjianlink/blog/pojo/Message.java
@@ -0,0 +1,68 @@
+package cn.chenjianlink.blog.pojo;
+
+import java.util.Date;
+
+/**
+ * 留言对象
+ */
+public class Message {
+    private Integer id;
+    //留言者ip
+    private String userIp;
+    //留言者名称
+    private String userName;
+    //留言内容
+    private String content;
+    //留言时间
+    private Date leaveMessageDate;
+    //留言状态
+    private Integer state;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserIp() {
+        return userIp;
+    }
+
+    public void setUserIp(String userIp) {
+        this.userIp = userIp;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getLeaveMessageDate() {
+        return leaveMessageDate;
+    }
+
+    public void setLeaveMessageDate(Date leaveMessageDate) {
+        this.leaveMessageDate = leaveMessageDate;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+}
